f5af0d57b856d0915d77e9f8a4e8e6e2e7d89c33
Chris <46230205+cmoeglin1@users.noreply.github.com>
2019-04-04 16:58:57 -0400

    fixed downloads

--- Build-SandboxEnv.ps1
+++ Build-SandboxEnv.ps1
@@ -1,7 +1,12 @@
+$swiftTarget = "C:\Sandbox\sysmonconfig-export.xml"
+$setupTarget = "C:\Sandbox\sandbox-setup.ps1"
+$swiftURI = "https://raw.githubusercontent.com/SwiftOnSecurity/sysmon-config/master/sysmonconfig-export.xml"
+$setupURI = "https://raw.githubusercontent.com/cmoeglin1/Sandbox-Config/master/sandbox-setup.ps1"
+$wc = New-Object -TypeName System.Net.WebClient
 if (test-path C:\Sandbox)
 {
 	del C:\Sandbox -force -recurse
 }
 copy \\live.sysinternals.com\tools\* C:\Sandbox\SysinternalsSuite
-wget https://raw.githubusercontent.com/SwiftOnSecurity/sysmon-config/master/sysmonconfig-export.xml > C:\Sandbox\sysmonconfig-export.xml
-wget https://raw.githubusercontent.com/cmoeglin1/Sandbox-Config/master/sandbox-setup.ps1 > C:\Sandbox\sandbox-setup.ps1
+$wc.DownloadFile($swiftURI,$swiftTarget)
+$wc.DownloadFile($setupURI,$setupTarget)

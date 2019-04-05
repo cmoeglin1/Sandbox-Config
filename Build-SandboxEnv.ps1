2ad35de96011808b2c9f3baaf674e2c5725a5da9
Chris <46230205+cmoeglin1@users.noreply.github.com>
2019-04-05 11:46:35 -0400

    Update Build-SandboxEnv.ps1
    Added npp install

--- Build-SandboxEnv.ps1
+++ Build-SandboxEnv.ps1
@@ -2,6 +2,9 @@ $swiftTarget = "C:\Sandbox\sysmonconfig-export.xml"
 $setupTarget = "C:\Sandbox\sandbox-setup.ps1"
 $swiftURI = "https://raw.githubusercontent.com/SwiftOnSecurity/sysmon-config/master/sysmonconfig-export.xml"
 $setupURI = "https://raw.githubusercontent.com/cmoeglin1/Sandbox-Config/master/sandbox-setup.ps1"
+$npptarget = "C:\sandbox\npp.exe"
+$nppuri = "https://notepad-plus-plus.org/repository/7.x/7.6.6/npp.7.6.6.Installer.x64.exe"
+
 $wc = New-Object -TypeName System.Net.WebClient
 if (test-path C:\Sandbox)
 {
@@ -10,3 +13,4 @@ if (test-path C:\Sandbox)
 copy \\live.sysinternals.com\tools\* C:\Sandbox\SysinternalsSuite
 $wc.DownloadFile($swiftURI,$swiftTarget)
 $wc.DownloadFile($setupURI,$setupTarget)
+$wc.downloadfile($nppuri,$npptarget)

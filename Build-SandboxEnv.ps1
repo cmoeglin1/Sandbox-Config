dc41a9a7f58b2ad0dc013b76b4a42d588afd3169
Chris Moeglin <cmoeglin1@gmail.com>
2021-07-12 19:38:09 -0400

    generalized sandbox location

--- Build-SandboxEnv.ps1
+++ Build-SandboxEnv.ps1
@@ -1,16 +1,18 @@
-$swiftTarget = "C:\Sandbox\sysmonconfig-export.xml"
-$setupTarget = "C:\Sandbox\sandbox-setup.ps1"
+$sandboxlocation = "C:\Sandbox"
+$desktopShortcut = $true
+$swiftTarget = "$sandboxLocation\sysmonconfig-export.xml"
+$setupTarget = "$sandboxLocation\sandbox-setup.ps1"
 $swiftURI = "https://raw.githubusercontent.com/SwiftOnSecurity/sysmon-config/master/sysmonconfig-export.xml"
 $setupURI = "https://raw.githubusercontent.com/cmoeglin1/Sandbox-Config/master/sandbox-setup.ps1"
-$npptarget = "C:\sandbox\npp.exe"
+$npptarget = "$sandboxLocation\npp.exe"
 $nppuri = "https://notepad-plus-plus.org/repository/7.x/7.6.6/npp.7.6.6.Installer.x64.exe"
 
 $wc = New-Object -TypeName System.Net.WebClient
-if (test-path C:\Sandbox)
+if (test-path $sandboxLocation)
 {
-	del C:\Sandbox -force -recurse
+	Remove-Item $sandboxLocation -force -recurse
 }
-copy \\live.sysinternals.com\tools\* C:\Sandbox\SysinternalsSuite
+Copy-Item \\live.sysinternals.com\tools\* $sandboxLocation\SysinternalsSuite
 $wc.DownloadFile($swiftURI,$swiftTarget)
 $wc.DownloadFile($setupURI,$setupTarget)
 $wc.downloadfile($nppuri,$npptarget)

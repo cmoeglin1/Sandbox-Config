8a07ab205b9fa9155084ec91297df62a3cb37b1c
Chris Moeglin <cmoeglin1@gmail.com>
2021-07-12 23:04:57 -0400

    lots o fixes - desktopshortcut added

--- Build-SandboxEnv.ps1
+++ Build-SandboxEnv.ps1
@@ -4,7 +4,10 @@ $swiftTarget = "$sandboxLocation\sysmonconfig-export.xml"
 $setupTarget = "$sandboxLocation\sandbox-setup.ps1"
 $swiftURI = "https://raw.githubusercontent.com/SwiftOnSecurity/sysmon-config/master/sysmonconfig-export.xml"
 $setupURI = "https://raw.githubusercontent.com/cmoeglin1/Sandbox-Config/master/sandbox-setup.ps1"
+$wsbURI = "https://raw.githubusercontent.com/cmoeglin1/Sandbox-Config/master/Sandbox.wsb"
+$wsbTarget = "$sandboxLocation\Sandbox.wsb"
 $npptarget = "$sandboxLocation\npp.exe"
+$sysTarget = "$sandboxLocation\Sysinternals"
 $nppuri = "https://notepad-plus-plus.org/repository/7.x/7.6.6/npp.7.6.6.Installer.x64.exe"
 
 $wc = New-Object -TypeName System.Net.WebClient
@@ -12,7 +15,14 @@ if (test-path $sandboxLocation)
 {
 	Remove-Item $sandboxLocation -force -recurse
 }
-Copy-Item \\live.sysinternals.com\tools\* $sandboxLocation\SysinternalsSuite
+New-Item $sandboxLocation -itemtype directory
+New-Item $sysTarget -itemtype directory
+Copy-Item \\live.sysinternals.com\tools\* $sysTarget -Recurse
 $wc.DownloadFile($swiftURI,$swiftTarget)
 $wc.DownloadFile($setupURI,$setupTarget)
+$wc.DownloadFile($wsbURI,$wsbTarget)
 $wc.downloadfile($nppuri,$npptarget)
+if ($desktopShortcut) {
+	$DesktopPath = [Environment]::GetFolderPath("Desktop")
+	Copy-Item -Path $wsbTarget "$DesktopPath\Sandbox.wsb"
+}

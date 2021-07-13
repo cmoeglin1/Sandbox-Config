8adbc55f3c28c1a33d80a4611af6507a22badf22
Chris Moeglin <cmoeglin1@gmail.com>
2021-07-13 02:15:50 -0400

    fixed path for sysmon

--- sandbox-setup.ps1
+++ sandbox-setup.ps1
@@ -1,2 +1,2 @@
 start-sleep 60
-C:\users\wdagutilityaccount\desktop\Sandbox\SysinternalsSuite\sysmon.exe -accepteula -i C:\users\wdagutilityaccount\desktop\Sandbox\sysmonconfig-export.xml
+C:\users\wdagutilityaccount\desktop\Sandbox\Sysinternals\sysmon.exe -accepteula -i C:\users\wdagutilityaccount\desktop\Sandbox\sysmonconfig-export.xml

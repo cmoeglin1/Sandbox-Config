2eb78f71a471ffa0ed7774d8d5672387ea21c977
Chris Moeglin <cmoeglin1@gmail.com>
2021-07-16 20:00:36 -0400

    started adding config file

--- Build-SandboxEnv.ps1
+++ Build-SandboxEnv.ps1
@@ -27,3 +27,7 @@ if ($desktopShortcut) {
 	$DesktopPath = [Environment]::GetFolderPath("Desktop")
 	Copy-Item -Path $wsbTarget "$DesktopPath\Sandbox.wsb"
 }
+
+#from http://tlingenf.spaces.live.com/blog/cns!B1B09F516B5BAEBF!213.entry
+#
+Get-Content "$PSScriptRoot\config.txt" | foreach-object -begin {$h=@{}} -process { $k = [regex]::split($_,'='); if(($k[0].CompareTo("") -ne 0) -and ($k[0].StartsWith("[") -ne $True)) { $h.Add($k[0], $k[1]) } }
